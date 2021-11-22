2ddc7ecc57141f290a4718c709079437d7d70a46
Arikawa <da20304001@ga.tera-house.ac.jp>
2021-11-22 13:58:57 +0900

    masterからarikawaに移行

---
+++ project/src/dao/AbstractDaoFactory.java
@@ -0,0 +1,47 @@
+package dao;
+
+
+import  java.io.FileInputStream;
+import  java.io.FileNotFoundException;
+import  java.io.IOException;
+import  java.util.Properties;
+
+public abstract class AbstractDaoFactory{
+    @SuppressWarnings("deprecation")
+	public static AbstractDaoFactory getFactory(){
+        AbstractDaoFactory factory=null;
+        Properties prop=new Properties();
+
+        try{
+
+
+        	prop.load( AbstractDaoFactory.class.getClassLoader().getResourceAsStream("properties/dao.properties"));
+        	//prop.load(new FileInputStream("C:/Users/koyama/git/project/project/src/properties/dao.properties"));
+
+
+            String name=prop.getProperty("dao");
+
+            Class c=Class.forName(name);
+
+            factory=(AbstractDaoFactory)c.newInstance();
+
+        } catch (FileNotFoundException e) {
+			// 実際には独自例外にラップしてスローする
+			throw new RuntimeException(e.getMessage(), e);
+		} catch (IOException e) {
+			// 実際には独自例外にラップしてスローする
+			throw new RuntimeException(e.getMessage(), e);
+		} catch (ClassNotFoundException e) {
+			// 実際には独自例外にラップしてスローする
+			throw new RuntimeException(e.getMessage(), e);
+		} catch (InstantiationException e) {
+			// 実際には独自例外にラップしてスローする
+			throw new RuntimeException(e.getMessage(), e);
+		} catch (IllegalAccessException e) {
+			// 実際には独自例外にラップしてスローする
+			throw new RuntimeException(e.getMessage(), e);
+		}
+        return factory;
+        }
+        public abstract ProductsDao getProductsDao();
+    }
