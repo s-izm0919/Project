8936502b200a0820e0516949df2edd9354979a0e
s-izm0919 <73629873+s-izm0919@users.noreply.github.com>
2021-11-29 16:06:38 +0900

    izumi

---
+++ project/src/filter/AuthenticatedFilter.java
@@ -0,0 +1,44 @@
+package filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+public class AuthenticatedFilter implements Filter {
+    public void init(FilterConfig config)throws ServletException{
+
+    }
+
+    public void destroy(){
+
+    }
+
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException{
+        HttpSession session = ((HttpServletRequest)req).getSession();
+
+        String flag = (String) session.getAttribute("token");
+
+        if(flag==null){
+
+            HttpServletRequest hreq =(HttpServletRequest)req;
+
+            String servletPath = hreq.getServletPath();
+
+            hreq.setAttribute("target",servletPath);
+
+            RequestDispatcher dis = req.getRequestDispatcher("/orderlogin");
+            dis.forward(req,res);
+
+        }else{
+            chain.doFilter(req,res);
+        }
+    }
+}
