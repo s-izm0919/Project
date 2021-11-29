717c8bf7b9c84f488fbd6429e36c520e264a2211
s-izm0919 <73629873+s-izm0919@users.noreply.github.com>
2021-11-29 14:05:23 +0900

    最新

--- project/src/bean/Favorite.java
+++ project/src/bean/Favorite.java
@@ -4,7 +4,7 @@ public class Favorite implements Serializable{
 	private String userId;
 	private int itemId;
 	private String favoriteDate;
-<<<<<<< HEAD
+
 	private int favoriteCount;	//いいね数
 
 	public Favorite() {
@@ -34,30 +34,4 @@ public class Favorite implements Serializable{
 	public void setFavoriteCount(int favoriteCount){
 		this.favoriteCount = favoriteCount;
 	}
-=======
-
-	public Favorite() {
-		super();
-	}
-	public String getUserId() {
-		return userId;
-	}
-	public void setUserId(String userId) {
-		this.userId = userId;
-	}
-	public int getItemId() {
-		return itemId;
-	}
-	public void setItemId(int itemId) {
-		this.itemId = itemId;
-	}
-	public String getFavoriteDate() {
-		return favoriteDate;
-	}
-	public void setFavoriteDate(String favoriteDate) {
-		this.favoriteDate = favoriteDate;
-	}
-
->>>>>>> branch 'yamamoto' of git@github.com:s-izm0919/Project.git
-
 }
