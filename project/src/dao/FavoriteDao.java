86b05f174c4ee076f087f18e1a62fd9f38b35787
Arikawa <da20304001@ga.tera-house.ac.jp>
2021-11-30 15:50:24 +0900

    謎のエラー直し

--- project/src/dao/FavoriteDao.java
+++ project/src/dao/FavoriteDao.java
@@ -2,10 +2,9 @@ package dao;
 
 import java.util.List;
 
-import bean.Favorite;
-
 public interface FavoriteDao {
 	public List getSearchItemFavoriteCount(String itemName);
 	public List getSearchShopFavoriteCount(String shopName);
 
 }
+//
