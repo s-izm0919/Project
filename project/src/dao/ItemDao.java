eb2d258a621d1f4f4f030b2206b0ece117738f89
yukina <yukina@H20304040>
2021-12-01 16:05:50 +0900

    ItemDao復元

--- project/src/dao/ItemDao.java
+++ project/src/dao/ItemDao.java
@@ -1,6 +1,13 @@
 package dao;
 import java.util.List;
+
+import bean.Item;
 public interface ItemDao {
     public List getItemsItemName(String iname);
     public List getItemsShopName(String sname);
+    public int getSearchItemCount(String itemname);
+    public int getSearchShopCount(String shopname);
+    public void addItem(Item item);
+    public void updateItem(Item item);
+    public void removeItem(int itemid);
 }
