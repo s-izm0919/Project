6a48d0c1710c84f14eff96fa9b4385e93abbfe16
Arikawa <da20304001@ga.tera-house.ac.jp>
2022-01-21 15:24:40 +0900

    SQL訂正

--- project/src/dao/MySQLUserOrderItemDetailsDao.java
+++ project/src/dao/MySQLUserOrderItemDetailsDao.java
@@ -21,11 +21,11 @@ public class MySQLUserOrderItemDetailsDao implements UserOrderItemDetailsDao{
         try{
         	cn = Connector.getInstance().beginTransaction();
 
-        	String sql= "SELECT os.purchase_date, od.order_id, os.total_payment, os.boost_amount, s.shop_name, s.shop_id, i.main_image_path, od.item_id, i.item_name, i.item_price, i.item_data_path \r\n" +
-        			"FROM orders AS os \r\n" +
-        			"INNER JOIN order_detail AS od ON os.order_id=od.order_id \r\n" +
-        			"INNER JOIN item AS i ON od.item_id=i.item_id \r\n" +
-        			"INNER JOIN shop AS s ON i.shop_id=s.shop_id \r\n" +
+        	String sql= "SELECT os.purchase_date, od.order_id, os.total_payment, os.boost_amount, s.shop_name, s.shop_id, i.main_image_path, od.item_id, i.item_name, i.item_price, i.item_data_path " +
+        			"FROM orders AS os " +
+        			"INNER JOIN order_detail AS od ON os.order_id=od.order_id " +
+        			"INNER JOIN item AS i ON od.item_id=i.item_id " +
+        			"INNER JOIN shop AS s ON i.shop_id=s.shop_id " +
         			"WHERE os.order_id=(SELECT order_id FROM order_detail WHERE item_id=(SELECT item_id FROM order_detail WHERE item_id='"+itemid+"' AND order_id IN (SELECT order_id FROM orders WHERE user_id='"+userid+"')));";
 
             st=cn.prepareStatement(sql);
