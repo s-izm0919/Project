4648aa281986ffacc6247d702f420ec12e26af1e
yukina <yukina@H20304040>
2022-01-21 15:34:20 +0900

    履歴関係Dao 山本

---
+++ project/src/bean/HistoryList.java
@@ -0,0 +1,81 @@
+package bean;
+
+import java.io.Serializable;
+
+public class HistoryList implements Serializable {
+	private String mainImagePath;
+	private String categoryName;
+	private int categoryId;
+	private String itemName;
+	private int itemId;
+	private String shopName;
+	private String shopId;
+	private int itemPrice;
+	private int favoriteCount;
+	private int searchCount;
+
+	public HistoryList() {
+		super();
+	}
+	public String getMainImagePath() {
+		return mainImagePath;
+	}
+	public void setMainImagePath(String mainImagePath) {
+		this.mainImagePath = mainImagePath;
+	}
+	public String getCategoryName() {
+		return categoryName;
+	}
+	public void setCategoryName(String categoryName) {
+		this.categoryName = categoryName;
+	}
+	public int getCategoryId() {
+		return categoryId;
+	}
+	public void setCategoryId(int categoryId) {
+		this.categoryId = categoryId;
+	}
+	public String getItemName() {
+		return itemName;
+	}
+	public void setItemName(String itemName) {
+		this.itemName = itemName;
+	}
+	public int getItemId() {
+		return itemId;
+	}
+	public void setItemId(int itemId) {
+		this.itemId = itemId;
+	}
+	public String getShopName() {
+		return shopName;
+	}
+	public void setShopName(String shopName) {
+		this.shopName = shopName;
+	}
+	public String getShopId() {
+		return shopId;
+	}
+	public void setShopId(String shopId) {
+		this.shopId = shopId;
+	}
+	public int getItemPrice() {
+		return itemPrice;
+	}
+	public void setItemPrice(int itemPrice) {
+		this.itemPrice = itemPrice;
+	}
+	public int getFavoriteCount() {
+		return favoriteCount;
+	}
+	public void setFavoriteCount(int favoriteCount) {
+		this.favoriteCount = favoriteCount;
+	}
+	public int getSearchCount() {
+		return searchCount;
+	}
+	public void setSearchCount(int searchCount) {
+		this.searchCount = searchCount;
+	}
+
+}
