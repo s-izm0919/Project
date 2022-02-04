a006f40e112bc509985fcf203c44a55585be93e4
Arikawa <da20304001@ga.tera-house.ac.jp>
2022-02-04 15:38:46 +0900

    Shop売り上げ修正

--- project/src/dao/OrdersDao.java
+++ project/src/dao/OrdersDao.java
@@ -1,8 +1,12 @@
 package dao;
 
+import java.util.Date;
+import java.util.List;
+
 import bean.Orders;
 
 public interface OrdersDao {
 	public void addOrder(Orders od);
 	public Orders getOederId(String userid,String shopid);
+	public List getShopEarning(String shopId,Date month);
 }
